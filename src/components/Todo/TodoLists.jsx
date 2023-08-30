18707fcfeab03f510d89b1234077bf220081e47c
Iamsaintm <p.leelayuthto@hotmail.com>
2023-08-30 17:19:04 +0700

    finish: TodoLists

--- src/components/Todo/TodoLists.jsx
+++ src/components/Todo/TodoLists.jsx
@@ -1,7 +1,24 @@
-import styles from "./TodoList.module.scss";
+import styles from "./TodoLists.module.scss";
+import { FaTrashAlt, FaPen } from "react-icons/fa";
 
 function TodoLists() {
-  return <div>TodoLists</div>;
+  return (
+    <ul>
+      <li className={styles.todo}>
+        <span className={styles.todo__checkbox}></span>
+        <p className={styles.todo__task}>TodoItem 1</p>
+        <span className={styles.todo__date}>30 AUG</span>
+        <div className={styles.todo__action}>
+          <span className={styles.todo__edit}>
+            <FaPen />
+          </span>
+          <span className={styles.todo__delete}>
+            <FaTrashAlt />
+          </span>
+        </div>
+      </li>
+    </ul>
+  );
 }
 
 export default TodoLists;
