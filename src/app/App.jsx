d82456bd54f77d9ebe36ea60a390c2408bfe1ea3
Iamsaintm <p.leelayuthto@hotmail.com>
2023-08-31 09:24:15 +0700

    finish: todolist css

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,82 +1,25 @@
-import "./App.scss";
-import {
-  FaInbox,
-  FaCalendar,
-  FaCalendarAlt,
-  FaChevronDown,
-} from "react-icons/fa";
-import Header from "../components/Header";
-import Lists from "../components/Lists";
-import TodoHeader from "../components/Todo/TodoHeader";
-import TodoCreate from "../components/Todo/TodoCreate";
-import TodoLists from "../components/Todo/TodoLists";
+// Dependencies
+import './App.scss';
+import AppBar from '../components/Common/AppBar/AppBar';
+import SideBar from '../components/SideBar/SideBar';
+import TodoHeader from '../components/Todo/TodoHeader';
+import TodoCreate from '../components/Todo/TodoCreate';
+import TodoLists from '../components/Todo/TodoLists';
+import { Button } from '../components/Common/Button/Button';
 
 function App() {
-  const generalList = [
-    {
-      id: 1,
-      text: "Inbox",
-      icon: <FaInbox />,
-      active: true,
-    },
-    {
-      id: 2,
-      text: "Today",
-      icon: <FaCalendar />,
-      active: false,
-    },
-    {
-      id: 3,
-      text: "Next 7 days",
-      icon: <FaCalendarAlt />,
-      active: false,
-    },
-  ];
-  const projectList = [
-    {
-      id: 1,
-      text: "Project-A",
-      icon: <FaInbox />,
-      active: true,
-    },
-    {
-      id: 2,
-      text: "Project-B",
-      icon: <FaInbox />,
-      active: false,
-    },
-  ];
   return (
-    <div className="todo">
-      <div className="todo__header">
-        <Header />
+    <div className='todo'>
+      <div className='todo__header'>
+        <AppBar />
       </div>
-      <div className="todo__sidebar">
-        <aside className="sidebar">
-          <section className="sidebar__category"></section>
-          <Lists data={generalList} />
-          <section className="sidebar__category">
-            <div className="accordion">
-              <div className="accordion__toggle">
-                <li className="accordion__item">
-                  <FaChevronDown className="accordion__item__icon accordion__item__active" />
-                  <p className="accordion__item__text">Project</p>
-                </li>
-              </div>
-
-              <Lists data={projectList} />
-            </div>
-          </section>
-        </aside>
+      <div className='todo__sidebar'>
+        <SideBar />
       </div>
-      <div className="todo__content">
-        <main className="todo__container">
-          {/* Header */}
+      <div className='todo__content'>
+        <main className='todo__container'>
           <TodoHeader />
-
-          {/* CreateTodo */}
           <TodoCreate />
-          {/* TodoLists */}
           <TodoLists />
         </main>
       </div>
@@ -85,10 +28,3 @@ function App() {
 }
 
 export default App;
-
-/* Challenge : Refactor
-- ให้ 2 section render UI ที่ ...
-  - Option A (2/5): ต่างกัน <Lists /> กับ <Accordion /> : Difficult
-  - Optoin B (4/5): render UI เดียวกัน เช่น <Lists />
-  - Option C (5/5): render <Lists /> ภายใต้ <Accordion> <Lists /> </Accoridion> // props.children
-*/
--- src/components/Todo/TodoHeader.jsx
+++ src/components/Todo/TodoHeader.jsx
@@ -1,14 +1,13 @@
-import styles from "./TodoHeader.module.scss";
+import styles from './TodoHeader.module.scss';
+// styles = { header : cssRules}
 
 function TodoHeader() {
   const today = new Date();
-  const options = { weekday: "short", day: "numeric", month: "short" };
+  const options = { day: 'numeric', weekday: 'short', month: 'short' };
   return (
     <div className={styles.header}>
       <h1 className={styles.header__text}>Inbox</h1>
-      <span className={styles.header__date}>
-        {today.toLocaleDateString("en-US", options)}
-      </span>
+      <span className={styles.header__date}>{today.toLocaleDateString('en-US', options)}</span>
     </div>
   );
 }
