7863acccb015ae2b4c185bd1b5be660bd6f2b9b8
Iamsaintm <p.leelayuthto@hotmail.com>
2023-09-01 17:00:26 +0700

    Finish: Edit and Delete

--- src/components/Todo/TodoForm.jsx
+++ src/components/Todo/TodoForm.jsx
@@ -1,7 +1,7 @@
-import { useState } from 'react';
-import { nanoid } from 'nanoid';
-import { Button } from '../Common/Button/Button';
-import styles from './TodoForm.module.scss';
+import { useState } from "react";
+import { nanoid } from "nanoid";
+import { Button } from "../Common/Button/Button";
+import styles from "./TodoForm.module.scss";
 /*
   props = {
     textSubmit : string
@@ -25,20 +25,26 @@ props = {
 */
 function TodoForm(props) {
   const [isError, setIsError] = useState(false);
-  const [taskInput, setTaskInput] = useState('');
+  const [taskInput, setTaskInput] = useState(props.oldTodo?.task || "");
 
   const handleChangeInput = function (event) {
     if (isError) setIsError(false);
     setTaskInput(event.target.value);
   };
 
+  // 2 Mode : Add or Edit
   const handleSubmit = function (event) {
     event.preventDefault();
-    if (taskInput.trim() === '') {
+    if (taskInput.trim() === "") {
       setIsError(true);
       return;
     }
-    props.addTodo(taskInput);
+
+    if (props.addTodo) props.addTodo(taskInput);
+    else if (props.oldTodo && props.editTodo) {
+      props.editTodo(props.oldTodo.id, { task: taskInput });
+    }
+
     props.setIsOpenForm(false);
   };
 
@@ -49,16 +55,23 @@ function TodoForm(props) {
     <form onSubmit={handleSubmit} className={styles.todo__form__container}>
       <input
         className={styles.todo__form__input}
-        placeholder='Task Name'
+        placeholder="Task Name"
         value={taskInput}
         onChange={handleChangeInput}
       />
       {/*Form Footer */}
       <div className={styles.todo__form__footer}>
-        {isError ? <p className={styles.todo__error}>Title is required</p> : null}
+        {isError ? (
+          <p className={styles.todo__error}>Title is required</p>
+        ) : null}
         <div className={styles.todo__form__buttons}>
-          <Button text='Cancel' active={false} type='button' onClick={handleCancel} />
-          <Button text={props.textSubmit} active={true} type='submit' />
+          <Button
+            text="Cancel"
+            active={false}
+            type="button"
+            onClick={handleCancel}
+          />
+          <Button text={props.textSubmit} active={true} type="submit" />
         </div>
       </div>
     </form>
